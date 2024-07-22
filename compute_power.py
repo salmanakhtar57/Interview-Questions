a78f83077ee9f4dac1160adaea2698717da62fad
salmanakhtar57 <salmanmaken57@gmail.com>
2024-07-22 21:10:24 +0500

    computer power

---
+++ compute_power.py
@@ -0,0 +1,13 @@
+"""
+reutrn the squar of a number using pow function.
+
+Example1 = 4 Output: 16
+Example2 = 8 Output: 64
+"""
+
+def computer_power(num):
+    power = pow(num, 2)
+    return power
+
+print(computer_power(4))
+print(computer_power(8))
